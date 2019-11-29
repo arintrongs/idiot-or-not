778623d81f840fc2dac35542a230820cc02ee8f6
Arin Trongsantipong <arintrongs@gmail.com>
2019-11-29 17:31:19 +0700

    add rabbit mq

--- check-ans/src/service_check.js
+++ check-ans/src/service_check.js
@@ -1,32 +1,32 @@
-const amqp = require('amqplib/callback_api');
+const amqp = require("amqplib/callback_api");
 
-const url =
- 'amqp://mnyqoqiy:HtoGI0IGi-EoLMc6YH9m4zN1d4o7remQ@cougar.rmq.cloudamqp.com/mnyqoqiy';
+const url = "amqp://guest:guest@rabbitmq:5672";
 
- const publishMsg = (msg) => {amqp.connect(url, function(error, connection) {
- if (error) {
-  throw error;
- }
- connection.createChannel(function(error1, channel) {
-  if (error1) {
-   throw error1;
-  }
+const publishMsg = msg => {
+  amqp.connect(url, function(error, connection) {
+    if (error) {
+      throw error;
+    }
+    connection.createChannel(function(error1, channel) {
+      if (error1) {
+        throw error1;
+      }
 
-  let queue = 'main';
-//   let msg = 'eye';
+      let queue = "main";
+      //   let msg = 'eye';
 
-  channel.assertQueue(queue, {
-   durable: false
+      channel.assertQueue(queue, {
+        durable: false
+      });
+      channel.sendToQueue(queue, Buffer.from(msg), {
+        persistent: true
+      });
+      console.log("Sent '%s'", msg);
+    });
+    setTimeout(function() {
+      connection.close();
+    }, 500);
   });
-  channel.sendToQueue(queue, Buffer.from(msg), {
-   persistent: true
-  });
-  console.log("Sent '%s'", msg);
- });
- setTimeout(function() {
-  connection.close();
- }, 500);
-});
-}
+};
 
-module.exports = publishMsg
+module.exports = publishMsg;
--- leaderboard/src/server.js
+++ leaderboard/src/server.js
@@ -1,8 +1,8 @@
-const Express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const Express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-const fs = require('fs');
+const fs = require("fs");
 
 const app = new Express();
 
@@ -10,92 +10,91 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', async (req, res) => {
-	try {
-		const response = fs.readFileSync('leaderboard.json', error => {
-			if (error) throw error;
-		});
-		res.status(200).send(JSON.parse(response));
-	} catch (error) {
-		res.status(400).send(error);
-	}
+app.get("/", async (req, res) => {
+  try {
+    const response = fs.readFileSync("leaderboard.json", error => {
+      if (error) throw error;
+    });
+    res.status(200).send(JSON.parse(response));
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 const PORT = process.env.PORT || 80;
 
 app.listen(PORT, error => {
-	if (!error) {
-		console.log(`Express is running on port: ${PORT}!`);
-		console.log('Ready to use!');
-	}
+  if (!error) {
+    console.log(`Express is running on port: ${PORT}!`);
+    console.log("Ready to use!");
+  }
 });
 
-const amqp = require('amqplib/callback_api');
+const amqp = require("amqplib/callback_api");
 
-const url =
-	'amqp://mnyqoqiy:HtoGI0IGi-EoLMc6YH9m4zN1d4o7remQ@cougar.rmq.cloudamqp.com/mnyqoqiy';
+const url = "amqp://guest:guest@rabbitmq:5672";
 
 try {
-	amqp.connect(url, function(error0, connection) {
-		if (error0) {
-			throw error0;
-		}
-		connection.createChannel(function(error1, channel) {
-			if (error1) {
-				throw error1;
-			}
-			var queue = 'main';
-			channel.assertQueue(queue, {
-				durable: false
-			});
-			channel.prefetch(1);
+  amqp.connect(url, function(error0, connection) {
+    if (error0) {
+      throw error0;
+    }
+    connection.createChannel(function(error1, channel) {
+      if (error1) {
+        throw error1;
+      }
+      var queue = "main";
+      channel.assertQueue(queue, {
+        durable: false
+      });
+      channel.prefetch(1);
 
-			channel.consume(queue, function(msg) {
-				try {
-					const result = JSON.parse(msg.content.toString());
-					let index = null;
-					const leaderboard = require('./leaderboard.json');
-					leaderboard.data.forEach((val, ind) => {
-						if (result.uid === val.uid) {
-							index = ind;
-						}
-					});
-					if (index != null) {
-						const user = leaderboard.data[index];
-						if (result.result) {
-							user.score = user.score + 100;
-						} else {
-							user.score = user.score - 150;
-						}
-						leaderboard.data[index] = user;
-					} else {
-						if (result.result) {
-							leaderboard.data.push({
-								uid: result.uid,
-								score: 100
-							});
-						} else {
-							leaderboard.data.push({
-								uid: result.uid,
-								score: -150
-							});
-						}
-					}
+      channel.consume(queue, function(msg) {
+        try {
+          const result = JSON.parse(msg.content.toString());
+          let index = null;
+          const leaderboard = require("./leaderboard.json");
+          leaderboard.data.forEach((val, ind) => {
+            if (result.uid === val.uid) {
+              index = ind;
+            }
+          });
+          if (index != null) {
+            const user = leaderboard.data[index];
+            if (result.result) {
+              user.score = user.score + 100;
+            } else {
+              user.score = user.score - 150;
+            }
+            leaderboard.data[index] = user;
+          } else {
+            if (result.result) {
+              leaderboard.data.push({
+                uid: result.uid,
+                score: 100
+              });
+            } else {
+              leaderboard.data.push({
+                uid: result.uid,
+                score: -150
+              });
+            }
+          }
 
-					leaderboard.data = leaderboard.data.sort((a, b) =>
-						a.score > b.score ? -1 : 1
-					);
+          leaderboard.data = leaderboard.data.sort((a, b) =>
+            a.score > b.score ? -1 : 1
+          );
 
-					const json = JSON.stringify(leaderboard);
-					fs.writeFile('leaderboard.json', json, err => {
-						if (err) throw err;
-					});
-				} catch (err) {}
+          const json = JSON.stringify(leaderboard);
+          fs.writeFile("leaderboard.json", json, err => {
+            if (err) throw err;
+          });
+        } catch (err) {}
 
-				setTimeout(function() {
-					channel.ack(msg);
-				}, 1000);
-			});
-		});
-	});
+        setTimeout(function() {
+          channel.ack(msg);
+        }, 1000);
+      });
+    });
+  });
 } catch (err) {}
