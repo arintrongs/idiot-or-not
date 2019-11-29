c5ea449c1b81f546cf946e1c227ee6aedbdaa29a
Arin Trongsantipong <arintrongs@gmail.com>
2019-11-29 15:34:25 +0700

    update before FINAL :fire:

--- api-gateway/main.go
+++ api-gateway/main.go
@@ -1,32 +1,36 @@
 package main
 
 import (
-		"fmt"
-		"log"
-		"os"
-		"encoding/json"
-		"net/http"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
 type Submit struct {
-	Uid  string `json:"uid"`
-	Ans  int		`json:"ans"`
+	Uid string `json:"uid"`
+	Ans int    `json:"ans"`
 }
 
 func leaderboard(w http.ResponseWriter, req *http.Request) {
-		resp, err := http.Get(os.Getenv("LEADERBOARD_URL"))
+	setupResponse(&w, req)
 
-		js, err := json.Marshal(resp)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+	resp, err := http.Get(os.Getenv("LEADERBOARD_URL"))
 
-		w.Header().Set("Content-Type", "application/json")
-    w.Write(js)
+	js, err := json.Marshal(resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
 
 func question(w http.ResponseWriter, req *http.Request) {
+	setupResponse(&w, req)
+
 	resp, err := http.Get(os.Getenv("QUESTION_URL"))
 
 	js, err := json.Marshal(resp)
@@ -37,42 +41,49 @@ func question(w http.ResponseWriter, req *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(js)
-    
+
 }
 
 func submit(w http.ResponseWriter, req *http.Request) {
-		decoder := json.NewDecoder(req.Body)
-		var s Submit
-		decoder.Decode(&s)
-		
+	setupResponse(&w, req)
 
-		json, err := json.Marshal(s)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		http.Post(os.Getenv("SUBMIT_URL"), "application/json", req.Body)
+	decoder := json.NewDecoder(req.Body)
+	var s Submit
+	decoder.Decode(&s)
 
-		w.Header().Set("Content-Type", "application/json")	
-		w.Write(json)
+	json, err := json.Marshal(s)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Post(os.Getenv("SUBMIT_URL"), "application/json", req.Body)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(json)
 }
 
 func test(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "Hii!!!!!!!")
+	fmt.Fprintf(w, "Hii!!!!!!!")
 }
 
 func failOnError(err error, msg string) {
-  if err != nil {
-    log.Fatalf("%s: %s", msg, err)
-  }
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func setupResponse(w *http.ResponseWriter, req *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
 func main() {
 
-		http.HandleFunc("/leaderboard", leaderboard)
-		http.HandleFunc("/question", question)
-    http.HandleFunc("/submit", submit)
-		http.HandleFunc("/test", test)
+	http.HandleFunc("/leaderboard", leaderboard)
+	http.HandleFunc("/question", question)
+	http.HandleFunc("/submit", submit)
+	http.HandleFunc("/test", test)
 
-		http.ListenAndServe(":8090", nil)
-		
+	http.ListenAndServe(":80", nil)
+
 }
