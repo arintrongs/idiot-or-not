24012b25ca6644e98c991537b0cb69c07a4453b1
Alxegore <eqsk134@gmail.com>
2019-11-30 14:10:50 +0700

    lEaDeRbOaRd

--- frontend/src/pages/HomePage/components/Leaderboard.jsx
+++ frontend/src/pages/HomePage/components/Leaderboard.jsx
@@ -1,15 +1,41 @@
 import React from 'react';
+import { Table, Divider, Tag } from 'antd';
 
 class Leaderboard extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: this.props.data || [],
+    };
+  }
+  
   render() {
+
+    const columns = [
+      {
+        title: 'Username',
+        dataIndex: 'uid',
+        key: 'uid',
+        render: text => <b>{text}</b>,
+      },
+      {
+        title: 'Score',
+        dataIndex: 'score',
+        key: 'score',
+      },
+    ];
+
     return (
       <div style={{padding: '40px', marginLeft: '60px'}}> 
       <div style={{fontSize: '36px'}}>Leaderboard </div>
-        {this.props.data.map((val,idx)=>{
+      <Table columns={columns} dataSource={this.props.data} />
+      
+        {/* {this.props.data.map((val,idx)=>{
       return(
         <div style={{padding: '20px', fontSize: '20px'}}> {val.uid}: {val.score} </div>
       ) 
-    })}
+    })} */}
       </div>
       
     )
