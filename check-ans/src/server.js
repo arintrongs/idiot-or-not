5ed7b2c23b424b65bf9abaf88673f07c929e46c8
Arin Trongsantipong <arintrongs@gmail.com>
2019-11-30 19:01:10 +0700

    fix res

--- check-ans/src/server.js
+++ check-ans/src/server.js
@@ -1,7 +1,7 @@
-const Express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const axios = require('axios');
+const Express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const app = new Express();
 
@@ -10,75 +10,68 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const getAns = (num, op) => {
-
-    for (var i = 0; i < num.length - 1; i++) {
-        if (op[i] === '^') {
-            num.splice(i, 2, num[i] ** num[i + 1])
-            op.splice(i, 1)
-        }
+  for (var i = 0; i < num.length - 1; i++) {
+    if (op[i] === "^") {
+      num.splice(i, 2, num[i] ** num[i + 1]);
+      op.splice(i, 1);
     }
+  }
 
-    for (var i = 0; i < num.length - 1; i++) {
-        if (op[i] === '*') {
-            num.splice(i, 2, num[i] * num[i + 1])
-            op.splice(i, 1)
-            i -= 1
-        }
-        else if (op[i] === '/') {
-            num.splice(i, 2, Math.floor(num[i] / (num[i + 1] + 0.0001)))
-            op.splice(i, 1)
-            i -= 1
-        }
+  for (var i = 0; i < num.length - 1; i++) {
+    if (op[i] === "*") {
+      num.splice(i, 2, num[i] * num[i + 1]);
+      op.splice(i, 1);
+      i -= 1;
+    } else if (op[i] === "/") {
+      num.splice(i, 2, Math.floor(num[i] / (num[i + 1] + 0.0001)));
+      op.splice(i, 1);
+      i -= 1;
     }
+  }
 
-    for (var i = 0; i < num.length - 1; i++) {
-        if (op[i] === '+') {
-            num.splice(i, 2, num[i] + num[i + 1])
-            op.splice(i, 1)
-            i -= 1
-
-        }
-        else if (op[i] === '-') {
-            num.splice(i, 2, num[i] - num[i + 1])
-            op.splice(i, 1)
-            i -= 1
-
-        }
+  for (var i = 0; i < num.length - 1; i++) {
+    if (op[i] === "+") {
+      num.splice(i, 2, num[i] + num[i + 1]);
+      op.splice(i, 1);
+      i -= 1;
+    } else if (op[i] === "-") {
+      num.splice(i, 2, num[i] - num[i + 1]);
+      op.splice(i, 1);
+      i -= 1;
     }
-    return num[0]
-}
+  }
+  return num[0];
+};
 
-app.post('/', async (req, res) => {
-    console.log('request to check-ans')
-    console.log(req.body)
-    try {
-        const ans = getAns(req.body.num, req.body.op)
-        let result = false
-        if (ans === req.body.ans) {
-            result = true
-        }
-        const payload = {
-            uid: req.body.uid,
-            result
-        }
-        console.log('res from check-ans')
-        console.log(payload)
-        res.status(200).send(payload)
-        await axios.post('http://leaderboard/update',payload)
-
+app.post("/", async (req, res) => {
+  console.log("request to check-ans");
+  console.log(req.body);
+  try {
+    const ans = getAns(req.body.num, req.body.op);
+    let result = false;
+    if (ans === req.body.ans) {
+      result = true;
     }
-    catch (error) {
-        console.log('res from check-ans')
-        console.log('error')
-        res.status(400).send(error);
-    }
-})
+    const payload = {
+      uid: req.body.uid,
+      result
+    };
+    console.log("res from check-ans");
+    console.log(payload);
+    await axios.post("http://leaderboard/update", payload);
+    return res.status(200).send(payload);
+  } catch (error) {
+    console.log("res from check-ans");
+    console.log("error");
+    return res.status(400).send(error);
+  }
+});
 
 const PORT = 80;
 
 app.listen(PORT, error => {
-    if (!error) {
-        console.log(`Express is running on port: ${PORT}!`);
-        console.log('Ready to use!');
-    }
+  if (!error) {
+    console.log(`Express is running on port: ${PORT}!`);
+    console.log("Ready to use!");
+  }
 });
--- gen-question/src/server.js
+++ gen-question/src/server.js
@@ -1,6 +1,6 @@
-const Express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const Express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = new Express();
 
@@ -9,105 +9,96 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const genRand = () => {
-    var ans = 10000
+  var ans = 10000;
 
-    while(ans>150 && ans > -100){
-        const oprand = ['+','-','*','/','^']
-        var randomlen = Math.floor(Math.random() * (5 - 3 + 1)) + 3;
-        var num = []
-        var op = []
-        var max = 10
-        var min = 0
+  while (ans > 150 && ans > -100) {
+    const oprand = ["+", "-", "*", "/", "^"];
+    var randomlen = Math.floor(Math.random() * (5 - 3 + 1)) + 3;
+    var num = [];
+    var op = [];
+    var max = 10;
+    var min = 0;
 
-        for(var i=0;i<randomlen;i++){
-            var item = oprand[Math.floor(Math.random()*oprand.length)];
-            var randomnum = Math.floor(Math.random() * (max - min + 1)) + min;
-            num.push(randomnum)
-            op.push(item)
-            max = 10
-            min = 0
-            if (item === '^'){
-                var tmp = 100
-                var c = 0 
-                do{
-                    tmp = Math.floor(tmp/(num[i]))-1
-                    c+=1
-                }while(tmp>1 && num[i]!=0)
-                max = c
-            }
-            else if (item === '/'){
-                min = 1
-            }
-        }
-        op.pop()
-        tmp = [...num]
-        ans = getAns(tmp,op)
-
+    for (var i = 0; i < randomlen; i++) {
+      var item = oprand[Math.floor(Math.random() * oprand.length)];
+      var randomnum = Math.floor(Math.random() * (max - min + 1)) + min;
+      num.push(randomnum);
+      op.push(item);
+      max = 10;
+      min = 0;
+      if (item === "^") {
+        var tmp = 100;
+        var c = 0;
+        do {
+          tmp = Math.floor(tmp / num[i]) - 1;
+          c += 1;
+        } while (tmp > 1 && num[i] != 0);
+        max = c;
+      } else if (item === "/") {
+        min = 1;
+      }
     }
-    return {
-        num : num,
-        ans : ans
+    op.pop();
+    tmp = [...num];
+    ans = getAns(tmp, op);
+  }
+  return {
+    num: num,
+    ans: ans
+  };
+};
+const getAns = (num, op) => {
+  for (var i = 0; i < num.length - 1; i++) {
+    if (op[i] === "^") {
+      num.splice(i, 2, num[i] ** num[i + 1]);
+      op.splice(i, 1);
     }
+  }
+  for (var i = 0; i < num.length - 1; i++) {
+    if (op[i] === "*") {
+      num.splice(i, 2, num[i] * num[i + 1]);
+      op.splice(i, 1);
+      i -= 1;
+    } else if (op[i] === "/") {
+      num.splice(i, 2, Math.floor(num[i] / (num[i + 1] + 0.0001)));
+      op.splice(i, 1);
+      i -= 1;
+    }
+  }
+  for (var i = 0; i < num.length - 1; i++) {
+    if (op[i] === "+") {
+      num.splice(i, 2, num[i] + num[i + 1]);
+      op.splice(i, 1);
+      i -= 1;
+    } else if (op[i] === "-") {
+      num.splice(i, 2, num[i] - num[i + 1]);
+      op.splice(i, 1);
+      i -= 1;
+    }
+  }
+  return num[0];
+};
 
-}
-const getAns = (num,op) => {
-
-    for(var i=0;i<num.length-1;i++){
-        if (op[i] === '^'){
-            num.splice(i,2,num[i]**num[i+1])
-            op.splice(i,1)
-        }
-    }
-    for(var i=0;i<num.length-1;i++){
-        if (op[i] === '*'){
-            num.splice(i,2,num[i]*num[i+1])
-            op.splice(i,1)
-            i -= 1
-        }
-        else if (op[i] === '/'){
-            num.splice(i,2,Math.floor(num[i]/(num[i+1]+0.0001)))
-            op.splice(i,1)
-            i -= 1
-        }
-    }
-    for(var i=0;i<num.length-1;i++){
-        if (op[i] === '+'){
-            num.splice(i,2,num[i]+num[i+1])
-            op.splice(i,1)
-            i -= 1
-
-        }
-        else if (op[i] === '-'){
-            num.splice(i,2,num[i]-num[i+1])
-            op.splice(i,1)
-            i -= 1
-
-        }
-    }
-    return num[0]
-}
-
-app.get('/', async(req,res)=>{
-    console.log('request to gen-question')
-    console.log(req.body)
-    try{
-        const rand = genRand()
-        console.log('res from gen-question')
-        console.log(JSON.stringify(rand))
-        res.status(200).send(JSON.stringify(rand))
-    }
-    catch(error){
-        console.log('res from gen-question')
-        console.log('error')
-        res.status(400).send(error);
-    }
-})
+app.get("/", async (req, res) => {
+  console.log("request to gen-question");
+  console.log(req.body);
+  try {
+    const rand = genRand();
+    console.log("res from gen-question");
+    console.log(JSON.stringify(rand));
+    return res.status(200).send(JSON.stringify(rand));
+  } catch (error) {
+    console.log("res from gen-question");
+    console.log("error");
+    return res.status(400).send(error);
+  }
+});
 
 const PORT = 80;
 
 app.listen(PORT, error => {
- if (!error) {
-  console.log(`Express is running on port: ${PORT}!`);
-  console.log('Ready to use!');
- }
+  if (!error) {
+    console.log(`Express is running on port: ${PORT}!`);
+    console.log("Ready to use!");
+  }
 });
