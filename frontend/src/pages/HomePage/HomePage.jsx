d5a65da8f97c91ea8344c4190fecf0c265c4658c
Alxegore <eqsk134@gmail.com>
2019-11-30 14:52:46 +0700

    interval leaderboard

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -2,18 +2,25 @@ import React from 'react';
 import { useFetch } from '../../utils/fetchData';
 import Problem from './components/Problem';
 import Leaderboard from './components/Leaderboard'
+import axios from "axios";
 
 import './HomePage.scss'
 
 const HomePage = () => {
 	const [dataLeaderboard, loadingLeaderboard] = useFetch('http://192.168.0.113:5000/leaderboard');
-	const [dataQuestion, loadingQuestion] = useFetch('http://192.168.0.113:5000/question');
+	let [dataQuestion, loadingQuestion] = useFetch('http://192.168.0.113:5000/question');
 	// const { num, ans } = dataQuestion
 	// const { data } = dataLeaderboard
 	console.log(dataQuestion)
 	const num = dataQuestion.num || [4, 5, 9, 1];
 	const ans = dataQuestion.ans || 7;
 	const data = dataLeaderboard.data || [{uid: 'eqsk134', score: 100}, {uid: 'pong', score: -2100}]
+
+	setInterval(async () => {
+		const response = await axios.get('http://192.168.0.113:5000/question');
+    dataQuestion = response.data.data
+		console.log('ques', dataQuestion);
+	}, 10000);
 
 	return (
 		<div>
