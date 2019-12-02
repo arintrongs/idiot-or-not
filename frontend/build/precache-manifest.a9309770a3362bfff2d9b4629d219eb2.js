d42865764acc8fd98ca465a0c1e9498cef20d1f2
Arin Trongsantipong <arintrongs@gmail.com>
2019-12-02 19:29:22 +0700

    build2

---
+++ frontend/build/precache-manifest.a9309770a3362bfff2d9b4629d219eb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6f457a0919b923ee38158608470e043",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ed4bfd3da77c70199087",
+    "url": "./static/css/2.65d0fb34.chunk.css"
+  },
+  {
+    "revision": "a8294b8ff5ae41b03603",
+    "url": "./static/css/main.546fd3b9.chunk.css"
+  },
+  {
+    "revision": "ed4bfd3da77c70199087",
+    "url": "./static/js/2.58d98d4d.chunk.js"
+  },
+  {
+    "revision": "a8294b8ff5ae41b03603",
+    "url": "./static/js/main.8645cd10.chunk.js"
+  },
+  {
+    "revision": "f85061363cc8ba7cc272",
+    "url": "./static/js/runtime-main.bf2ccd9f.js"
+  }
+]);
